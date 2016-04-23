a95cf715a918ea743f5ab16aef8c4803a90613ec
Lankovski <lankovski.674@gmail.com>
2016-04-23 11:36:28 +0800

    new journey: reconstructing geobase data structures for GeoSMART, with new comprehension of FGeo3D demands

---
+++ vs2015/GeoObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FGeo3D_TE
+{
+    class GeoObject
+    {
+        string ID { get; set; }
+        string Name { get; set; }
+    }
+}
---
+++ vs2015/GeoTecture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FGeo3D_TE
+{
+    public enum TectureType : byte
+    {
+        //层面，层理，节理，断层，软弱夹层
+        BeddingSurface, Bedding, Joint, Fault, WeakInterbed
+    };
+    class GeoTecture
+    {
+        TectureType Tecture { get; set; } //构造类型
+        string TectureData { get; set; } //构造类型信息
+        GeoAttitude Attitude { get; set; } //构造产状
+
+        GeoTecture(TectureType tectureType, string tectureData, GeoAttitude attitude)
+        {
+            Tecture = tectureType;
+            TectureData = tectureData;
+            Attitude = attitude;
+        }
+    }
+}
