5054f7ab75d52324f67c1b31248638ebd390b19d
Lankovski <lankovski.674@gmail.com>
2016-06-17 21:11:02 +0800

    废除GeoMarkPoint Class

--- FGeo3D/GeoObjs/GeoMarkPoint.cs
+++ FGeo3D/GeoObjs/GeoMarkPoint.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using GeoIM.CHIDI.DZ.COM;
 using TerraExplorerX;
 
 namespace FGeo3D_TE
 {
+    //废
     class GeoMarkPoint:GeoPoint
     {
-        public GeoTecture Tecture { get; set; }
+        //public GeoTecture Tecture { get; set; }
+        public string MarkerType { get; set; }
+        public string MarkerData { get; set; }
 
-        public GeoMarkPoint(double x, double y, double h, string name, GeoTecture tecture)
-            : base(x, y, h, name)
-        {
-            Tecture = tecture;
-        }
+        public double Dip { get; set; }
+        public double DipAngle { get; set; }
+
+
+
 
         //绘制Mark信息（几何 + 产状）
         public override void Draw(ref SGWorld66 sgworld)
